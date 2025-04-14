85a21d3a873f565e89b3f56d7816a2257d6ef417
Chol211 <dengmathewe01@gmail.com>
2025-04-14 11:45:27 +0300

    Chore: Implementing state changes

--- Kakuma-Resource-Tracker-master/app.js
+++ Kakuma-Resource-Tracker-master/app.js
@@ -4,3 +4,8 @@ const resourceList=document.getElementById("resourceList");
 const counter=document.getElementById("counter");
 const searchInput=document.querySelector(".search-input");
 const filterButton=document.querySelectorAll;(".filter-btn");
+
+//This section demonstrates how to handle a state management in JS
+let resources=JSON.parse(localStorage.getItem("resources"));
+let currentFilter="All";
+let searchTerm="";
