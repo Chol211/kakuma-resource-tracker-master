ed0ab49978cdf9786f84bef61ee6a93f4a339758
Chol211 <dengmathewe01@gmail.com>
2025-04-14 11:34:22 +0300

    Chore:Grabbed DOM Elements

---
+++ Kakuma-Resource-Tracker-master/app.js
@@ -0,0 +1,6 @@
+//This section demostrates how DOM Elements
+const form=document.getElementById("resourceForm");
+const resourceList=document.getElementById("resourceList");
+const counter=document.getElementById("counter");
+const searchInput=document.querySelector(".search-input");
+const filterButton=document.querySelectorAll;(".filter-btn");
